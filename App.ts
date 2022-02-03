6e279e1d71912f36fedf8083d9b07dd6a768cb18
David <dav2070@web.de>
2022-02-03 23:13:16 +0100

    Add route for redirect to UniversalSoundboard

--- App.ts
+++ App.ts
@@ -17,6 +17,26 @@ export class App {
 			res.send('Nothing to see here')
 		})
 
+		router.get('/redirect', (req, res) => {
+			// TODO: Check the referer header for Stripe
+			console.log(req.headers.referer)
+
+			// Get the necessary url params
+			let success = req.query.success == "true"
+			let plan = +req.query.plan
+
+			if (
+				success == null
+				|| plan == null
+			) {
+				res.status(400).end()
+				return
+			}
+
+			// Open UniversalSoundboard
+			res.redirect(`universalsoundboard://accountpage?success=${success}&plan=${plan}`)
+		})
+
 		this.express.use('/', router)
 	}
 }
--- dist/server.js
+++
@@ -1,9 +0,0 @@
-import { App } from './App.js';
-const port = process.env.PORT || 3000;
-const app = new App();
-app.express.listen(port, (err) => {
-    if (err) {
-        return console.log(err);
-    }
-    return console.log(`server is listening on ${port}`);
-});
