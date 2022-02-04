c42036aa585d8e8640173427c061f2627c151c7e
David <dav2070@web.de>
2022-02-04 22:14:58 +0100

    Add basic UpgradePage with functionality for redirecting USB

---
+++ src/pages/upgrade-page/upgrade-page.ts
@@ -0,0 +1,12 @@
+window.addEventListener("load", main)
+
+function main() {
+	// Get the url params
+	const urlParams = new URLSearchParams(window.location.search)
+	let success = urlParams.get("success") == "true"
+	let plan = +urlParams.get("plan")
+
+	setTimeout(() => {
+		window.location.href = `universalsoundboard://upgrade?success=${success}&plan=${plan}`
+	}, 3000)
+}
