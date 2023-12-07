f98fd7fc81d2859cd16cd61c8522c99a117d4eef
David <zurek.dav@gmail.com>
2023-12-07 23:38:30 +0100

    Make small improvements on UpgradePage

--- src/pages/upgrade-page/upgrade-page.ts
+++ src/pages/upgrade-page/upgrade-page.ts
@@ -4,7 +4,7 @@ function main() {
 	// Get the url params
 	const urlParams = new URLSearchParams(window.location.search)
 	let success = urlParams.get("success") == "true"
-	let plan = +urlParams.get("plan")
+	let plan = Number(urlParams.get("plan"))
 
 	if (success) {
 		window.location.href = `universalsoundboard://upgrade?plan=${plan}`
